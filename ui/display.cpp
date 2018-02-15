42592f0eac4f6454d1aeaa1fc293f1f4461e2a7c
hellozee <hellozee@disroot.org>
2018-02-15 23:58:22 +0530

    set the opengl context to 3.3 and added a Recipe File

--- ui/display.cpp
+++ ui/display.cpp
@@ -13,6 +13,9 @@ Display::Display(int width, int height, const std::string &title)
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,32);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
     _window = SDL_CreateWindow(title.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_OPENGL);
     _glContext = SDL_GL_CreateContext(_window);
 
